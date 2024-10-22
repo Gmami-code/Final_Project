7ed88206e9a3f4255c94d0a13e976d598e745887
Gerania Cedeno <geraniaceden@umass.edu>
2024-10-22 11:36:44 -0400

    Update final_connect4.py

--- final_connect4.py
+++ final_connect4.py
@@ -6,6 +6,10 @@ def make_empty_board(nrows, ncols):
 # nrows = number of rows, strings
 # ncols = number of columns, "."
      return ["." * ncols for elem in range(nrows)]
+# return ^ generates a list that creates a list of strings
+# for return ^ , the string will repeat the "." character ncols times (7)
+# for return ^ , range(nrows) generates a sequence from 0 to nrows -1 (range(start at, stop before (nrows input - 1), step +1)
+# for return ^ , elem is a placeholder to represent each number inside the range 
 # "." = empty spot
 print(make_empty_board(6, 7))
 # print(make_empty_board(number of rows you want, number of columns you want))
